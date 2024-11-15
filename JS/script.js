a35d905fc6473a8554e748feed677d8db83676c2
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-11-15 21:12:56 +0530

    Add classes, JS code for darkmode functionality

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,11 @@ function darkMode(){
     let darkModeButtonElement = document.querySelector('.js-darkMode');
     if(darkModeButtonElement.innerText === 'Dark'){
         darkModeButtonElement.innerHTML = 'Light';
+        document.querySelector('.js-mainBg').classList.add('mainBg-dark');
+        document.querySelector('.js-output').classList.add('mainBg-dark');
     }else{
         darkModeButtonElement.innerHTML = 'Dark';
+        document.querySelector('.js-mainBg').classList.remove('mainBg-dark');
+        document.querySelector('.js-output').classList.remove('mainBg-dark');
     }
 }
