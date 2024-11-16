f0a5e1be9dd286b977e10277e202d6dfaf92b9cd
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-11-16 07:43:16 +0530

    Add darkmode color scheme to all buttons

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,16 @@ function darkMode(){
         darkModeButtonElement.innerHTML = 'Light';
         document.querySelector('.js-mainBg').classList.add('mainBg-dark');
         document.querySelector('.js-output').classList.add('mainBg-dark');
+        document.querySelectorAll('.js-greenOnGreyBg').forEach(button =>{button.classList.add('greenOnGreyBg-dark')});
+        document.querySelectorAll('.js-blackOnWhiteBg').forEach(button =>{button.classList.add('blackOnWhiteBg-dark')});
+        document.querySelectorAll('.js-redOnGrey').forEach(button =>{button.classList.add('redOnGrey-dark')});
     }else{
         darkModeButtonElement.innerHTML = 'Dark';
         document.querySelector('.js-mainBg').classList.remove('mainBg-dark');
         document.querySelector('.js-output').classList.remove('mainBg-dark');
+        document.querySelectorAll('.js-greenOnGreyBg').forEach(button =>{button.classList.remove('greenOnGreyBg-dark')});
+        document.querySelectorAll('.js-blackOnWhiteBg').forEach(button =>{button.classList.remove('blackOnWhiteBg-dark')});
+        document.querySelectorAll('.js-redOnGrey').forEach(button =>{button.classList.remove('redOnGrey-dark')});
     }
 }
 let calculation = '';
